77cd90926ac00020fd84db84ac4a34b83e2bbf13
João Roberto de França Pereira <joao.roberto@academico.ifrn.edu.br>
2019-01-10 11:31:34 -0300

    Início do Projeto

---
+++ src/test/java/br/com/helpc/etruck/EtruckApiApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.helpc.etruck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EtruckApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
